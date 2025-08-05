5c9dd20dd53be1a016cf5ee7e4e40c356e2ed92d
Assanfakou <fakouhasan@gmail.com>
2025-08-05 18:41:05 +0100

    moving the herdoc func to it's file and minimize it

---
+++ execution/herdoc_executter.c
@@ -0,0 +1,44 @@
+#include "execution.h"
+
+int read_dev_random(char **file)
+{
+	int read_fd;	
+
+	read_fd = open("/dev/random", O_RDONLY, 0642);
+	if (read_fd < 0)
+		return (1);
+	*file = malloc(8);
+	if (read(read_fd, *file, 8) == -1)
+	{
+		free(*file);
+		return (1);
+	}
+	close(read_fd);
+	return (0);
+}
+
+void handle_heardoc(char *delemeter)
+{
+	int fd;
+	char *filename;
+
+	if (read_dev_random(&filename))
+		return ;
+	fd = open(filename, O_WRONLY | O_CREAT, 0642);
+	if (fd < 0)
+	{
+		free(filename);
+		return;
+	}
+	write(fd, delemeter, ft_strlen(delemeter));
+	close(fd);
+	fd = open(filename, O_RDONLY, 0642);
+	if (fd < 0)
+	{
+		free(filename);
+		return;
+	}
+	dup_fd_inp(fd);
+	unlink(filename);
+	free(filename);
+}
